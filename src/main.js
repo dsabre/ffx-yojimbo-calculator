61ae9443a5a38e1ecc974779a3c3534b8c4f7174
Daniele Sabre <daniele.sabre@gmail.com>
2022-05-16 16:29:08 +0200

    Develop

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/css/style.css';
 
 const app = createApp(App)
-
-app.use(createPinia())
 
 app.mount('#app')
