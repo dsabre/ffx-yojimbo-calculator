567f5e1113c490105325e681cc335974810ab490
Daniele Sabre <daniele.sabre@gmail.com>
2022-05-16 17:35:56 +0200

    Develop

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// publicPath: process.env.NODE_ENV === 'production' ? '/ffx-yojimbo-calculator/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: ""
 })
